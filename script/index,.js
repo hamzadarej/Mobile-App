2e08cd3a011bdd915da86531eb2b547b81eb561e
hamza <darejhamza@gmail.com>
2021-03-18 10:35:33 +0100

    animation & x-menu

---
+++ script/index,.js
@@ -0,0 +1,10 @@
+let clicked = false;
+document.querySelector("xmenu").addEventListener("click", () => {
+  clicked = !clicked;
+  console.log(clicked);
+  if (clicked) {
+    document.querySelector("nav").style.cssText = "display:flex;";
+  } else {
+    document.querySelector("nav").style.cssText = "display:none ;";
+  }
+});
