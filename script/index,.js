e97477da9021be1592caa6353b2361564fdce63b
hamza <darejhamza@gmail.com>
2021-03-18 12:14:40 +0100

    animation & x-menu

--- script/index,.js
+++ script/index,.js
@@ -4,9 +4,10 @@ document.querySelector(".xmenu").addEventListener("click", () => {
   console.log(clicked);
   if (clicked) {
     document.querySelector("nav").style.cssText = "display:flex;";
+    document.querySelector(".topnav").style.cssText =
+      "justify-content: center;";
   } else {
     document.querySelector("nav").style.cssText = "display:none ;";
     document.querySelector(".topnav").style.cssText = "justify-content: left;";
-    
   }
 });
